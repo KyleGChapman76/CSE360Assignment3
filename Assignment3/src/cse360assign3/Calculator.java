87d0bb5227fe0e1d5a3e5ebe4ce9c96b985ba34f
Kyle Chapman <xeuorux@gmail.com>
2016-02-22 15:08:34 -0700

    Calculator method  and JUnit test case implementation
    Implementation and JUnit tests for all but the history method of the Calculator class.

--- Assignment3/src/cse360assign3/Calculator.java
+++ Assignment3/src/cse360assign3/Calculator.java
@@ -9,49 +9,63 @@ public class Calculator{
 	private int total;
 	
 	/* Creates an instance of the calculator class, initializing the running total to 0 */
-	public Calculator () {
+	public Calculator ()
+	{
 		total = 0;  // not needed - included for clarity
 	}
 	
 	/* Returns the current running total
 	 * @return int the current running total 
 	 */
-	public int getTotal () {
-		return 0;
+	public int getTotal ()
+	{
+		return total;
 	}
 	
 	/* Adds the given int value to the running total
 	 * @param value The integer value to add to the total
 	 */
-	public void add (int value) {
-		
+	public void add (int value)
+	{
+		total += value;
 	}
 	
 	/* Subtracts the given int value from the running total
 	 * @param value The integer value to subtract from the total
 	 */
-	public void subtract (int value) {
-		
+	public void subtract (int value)
+	{
+		total -= value;
 	}
 	
 	/* Multiplies the running total by the given int value
 	 * @param value The integer value to multiply the total by
 	 */
-	public void multiply (int value) {
-		
+	public void multiply (int value)
+	{
+		total *= value;
 	}
 	
 	/* Divides the running total by the given int value. Rounds the the total down.
 	 * @param value The integer value to divide the total by
 	 */
-	public void divide (int value) {
-		
+	public void divide (int value)
+	{
+		if (value == 0)
+		{
+			total = 0;
+		}
+		else
+		{
+			total /= value;
+		}
 	}
 	
 	/* Returns a string that represents the history of operations applied to the running total
 	 * @return String The string representation of the calculator's history
 	 */
-	public String getHistory () {
+	public String getHistory ()
+	{
 		return "";
 	}
 }
--- Assignment3/src/cse360assign3/CalculatorTest.java
+++ Assignment3/src/cse360assign3/CalculatorTest.java
@@ -4,41 +4,89 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-public class CalculatorTest {
+public class CalculatorTest
+{
 
 	@Test
-	public void testCalculator() {
+	public void testCalculator()
+	{
 		Calculator calulatorInstance = new Calculator();
 		assertNotNull(calulatorInstance);
 	}
 
 	@Test
-	public void testGetTotal() {
-		fail("Not yet implemented");
+	public void testGetTotal()
+	{
+		Calculator calulatorInstance = new Calculator();
+		assertEquals(calulatorInstance.getTotal(),0);
+		calulatorInstance.add(5);
+		assertEquals(calulatorInstance.getTotal(),5);
+		calulatorInstance.subtract(5);
+		assertEquals(calulatorInstance.getTotal(),0);
 	}
 
 	@Test
-	public void testAdd() {
-		fail("Not yet implemented");
+	public void testAdd()
+	{
+		Calculator calulatorInstance = new Calculator();
+		assertEquals(calulatorInstance.getTotal(),0);
+		calulatorInstance.add(5);
+		assertEquals(calulatorInstance.getTotal(),5);
+		calulatorInstance.add(15);
+		assertEquals(calulatorInstance.getTotal(),20);
+		calulatorInstance.add(-40);
+		assertEquals(calulatorInstance.getTotal(),-20);
 	}
 
 	@Test
-	public void testSubtract() {
-		fail("Not yet implemented");
+	public void testSubtract()
+	{
+		Calculator calulatorInstance = new Calculator();
+		assertEquals(calulatorInstance.getTotal(),0);
+		calulatorInstance.subtract(5);
+		assertEquals(calulatorInstance.getTotal(),-5);
+		calulatorInstance.subtract(15);
+		assertEquals(calulatorInstance.getTotal(),-20);
+		calulatorInstance.subtract(-40);
+		assertEquals(calulatorInstance.getTotal(),20);
 	}
 
 	@Test
-	public void testMultiply() {
-		fail("Not yet implemented");
+	public void testMultiply()
+	{
+		Calculator calulatorInstance = new Calculator();
+		assertEquals(calulatorInstance.getTotal(),0);
+		calulatorInstance.multiply(5);
+		assertEquals(calulatorInstance.getTotal(),0);
+		
+		calulatorInstance.add(5);
+		assertEquals(calulatorInstance.getTotal(),5);
+		
+		calulatorInstance.multiply(6);
+		assertEquals(calulatorInstance.getTotal(),30);
+		calulatorInstance.multiply(-2);
+		assertEquals(calulatorInstance.getTotal(),-60);
 	}
 
 	@Test
-	public void testDivide() {
-		fail("Not yet implemented");
+	public void testDivide()
+	{
+		Calculator calulatorInstance = new Calculator();
+		
+		calulatorInstance.add(50);
+		assertEquals(calulatorInstance.getTotal(),50);
+		
+		calulatorInstance.divide(5);
+		assertEquals(calulatorInstance.getTotal(),10);
+		calulatorInstance.divide(3);
+		assertEquals(calulatorInstance.getTotal(),3);
+		calulatorInstance.divide(0);
+		assertEquals(calulatorInstance.getTotal(),0);
 	}
 
 	@Test
-	public void testGetHistory() {
+	public void testGetHistory()
+	{
 		fail("Not yet implemented");
 	}
 
